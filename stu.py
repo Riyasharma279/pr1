b193d12743cf78d4b1c970a3f0cfc43e93209523
Riyasharma279 <138498348+Riyasharma279@users.noreply.github.com>
2023-08-28 21:22:58 +0530

    Create stu.py
    assignment done

---
+++ stu.py
@@ -0,0 +1,19 @@
+fdef print_positive_numbers(numbers_list):
+    positive_numbers = [num for num in numbers_list if num > 0]
+    print(positive_numbers)
+
+# User input for list1
+input_str = input("Enter a list of numbers separated by spaces: ")
+list1 = [int(num) for num in input_str.split()]
+
+print("Input:", list1)
+print("Output:", end=" ")
+print_positive_numbers(list1)
+
+# User input for list2
+input_str = input("\nEnter another list of numbers separated by spaces: ")
+list2 = [int(num) for num in input_str.split()]
+
+print("Input:", list2)
+print("Output:", end=" ")
+print_positive_numbers(list2)
